0aa528e4cd0c83c9e7dd8a394b4ebcec12a39a5d
justus <jfrausto10@gmail.com>
2021-05-05 16:47:12 -0600

    added animation as well as toggle function

--- labFile/labDemo.c
+++ labFile/labDemo.c
@@ -2,8 +2,10 @@
 #include <libTimer.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
+#include "switches.h"
+#include "led.h"
 
-#define LED_GREEN BIT6             // P1.6
+#define LED_RED BIT6             // P1.6
 
 
 short redrawScreen = 1;
@@ -14,7 +16,7 @@ void wdt_c_handler()
   static int secCount = 0;
 
   secCount ++;
-  if (secCount == 25) {
+  if (secCount == 100) {
     secCount = 0;
     fontFgColor = (fontFgColor == COLOR_GREEN) ? COLOR_BLACK : COLOR_GREEN;
     redrawScreen = 1;
@@ -24,10 +26,12 @@ void wdt_c_handler()
 
 void main()
 {
-  P1DIR |= LED_GREEN;/**< Green led on when CPU on */
-  P1OUT |= LED_GREEN;
+  P1DIR |= LED_RED;/**< red led on when CPU on */
+  P1OUT |= LED_RED;
   configureClocks();
   lcd_init();
+  led_init();
+  switch_init();
 
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);                 /**< GIE (enable interrupts) */
@@ -35,21 +39,23 @@ void main()
   static char posTracker = 0;
 
   clearScreen(COLOR_BLACK);
-  while (1) {/* forever */
+  while (1) {
     if (redrawScreen) {
       redrawScreen = 0;
-      drawString11x16(20,20,"hello",COLOR_GREEN,COLOR_RED);
-      if(posTracker<=20){
-	fillFlagPole(20+posTracker,45);
-	posTracker++;
-      }
-      else{
-	fillFlagPole(20,45);
-	posTracker = 0;
+      drawString11x16(20,20,"Lab 1",COLOR_GREEN,COLOR_RED);
+      if(switch_state_down1){
+	if(posTracker<=20){
+	  fillFlagPole(20+posTracker,45);
+	  posTracker++;
+	}
+	else{
+	  fillFlagPole(20,45);
+	  posTracker = 0;
+	}
       }
     }
-    P1OUT &= ~LED_GREEN;/* green off */
+    P1OUT &= ~LED_RED;/* green off */
     or_sr(0x10);/**< CPU OFF */
-    P1OUT |= LED_GREEN;/* green on */
+    P1OUT |= LED_RED;/* green on */
   }
 }
