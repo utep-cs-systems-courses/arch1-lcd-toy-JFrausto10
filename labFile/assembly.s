109e40aeef8456e5853f710619d14baa92405105
justus <jfrausto10@gmail.com>
2021-05-06 01:59:00 -0600

    added .s file that preforms the auto_backround_color

---
+++ labFile/assembly.s
@@ -0,0 +1,56 @@
+	.data
+state:
+	.word 0
+	
+	.text
+	
+	.extern posTracker      ;posTracker is type 'char'
+	
+jt:
+	.word default
+	.word case0
+	.word case5
+	.word case10
+	.word case15
+
+autoColorAssembly:
+	add #1, state
+	cmp #0, &posTracker     ; posTracker get compared against all casses
+	jz colorChooser
+	
+	add #1, state
+	cmp #5, &posTracker
+	jz colorChooser
+	
+	add #1, state
+	cmp #10, &posTracker
+	jz colorChooser
+	
+	add #1, state
+	cmp #15, &posTracker
+	jz colorChooser
+	
+	jmp jt(0)
+
+colorChooser:               
+	move &state, r13        ; state should equal proper case and pushed into r13
+	move jt(r13), r0        ; jump into jt(r13)
+	
+case0:
+	mov &COLOR_BLACK, r12
+	jmp end
+case5:
+	mov &COLOR_RAZZLE_DAZZLE, r12
+	jmp end	
+case10:
+	mov &COLOR_GOLD, r12
+	jmp end
+case15:
+	mov &COLOR_AQUAMARINE, r12
+	jmp end
+default:
+	jmp end
+end:
+	pop r0
+
+	
